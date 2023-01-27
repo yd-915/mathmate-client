c70385057c7f92f58c891da4dd91d4c1092bed92
Erez Birenholz <biran330@gmail.com>
2023-01-27 15:18:23 +0200

    Add MuiChipsInput to the searchResult

--- src/pages/SearchResultPage.tsx
+++ src/pages/SearchResultPage.tsx
@@ -10,30 +10,42 @@ import {
 import Navbar from "../components/navbar";
 import { initialQuestionObject, Question } from "../typescript";
 import SearchInput from "../components/searchInput";
+import { MuiChipsInputChip, MuiChipsInput } from "mui-chips-input";
+import { useState } from "react";
 
 const TEST_CONTENT = `Natus beatae eaque adipisci perspiciatis aliquam et enim sed et enim sed 
 aliquam et enim sed et enim sed jsfgku
 occaecati beatae eaque adipisc....`;
 
-function SearchResult() {
-  const questions: Question[] = [
-    {
-      ...initialQuestionObject,
-      title: "QUESTION TITLE 1",
-      question: TEST_CONTENT,
-    },
-    {
-      ...initialQuestionObject,
-      title: "QUESTION TITLE 2",
-      question: TEST_CONTENT,
-    },
-    {
-      ...initialQuestionObject,
-      title: "QUESTION TITLE 3",
-      question: TEST_CONTENT,
-    },
-  ];
+const questions: Question[] = [
+  {
+    ...initialQuestionObject,
+    title: "QUESTION TITLE 1",
+    question: TEST_CONTENT,
+  },
+  {
+    ...initialQuestionObject,
+    title: "QUESTION TITLE 2",
+    question: TEST_CONTENT,
+  },
+  {
+    ...initialQuestionObject,
+    title: "QUESTION TITLE 3",
+    question: TEST_CONTENT,
+  },
+];
 
+
+function SearchResult() {
+
+  const [tags, setTags] = useState<MuiChipsInputChip[]>(["aaaaa","bbbbbb"]);
+  const [text, setText] = useState("");
+
+  const handleChange = (newTags: MuiChipsInputChip[]) => {
+    setTags(newTags);
+  };
+
+ 
   return (
     <>
       <Navbar />
@@ -41,6 +53,13 @@ function SearchResult() {
       <Container sx={{ my: 2, justifyContent: "flex-start" }}>
         <SearchInput />
       </Container>
+
+      <MuiChipsInput
+          value={tags}
+          onChange={handleChange}
+          fullWidth
+          sx={{ mb: 2 }}
+        />
 
       <Stack spacing={2}>
         {questions.map((question) => (
