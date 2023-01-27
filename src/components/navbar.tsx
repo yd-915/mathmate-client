514f477e649541a495bacd63641bdffb1ab7b654
Erez Birenholz <biran330@gmail.com>
2023-01-27 15:39:49 +0200

    Add github icon for the navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,6 +4,7 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
+import GitHubIcon from '@mui/icons-material/GitHub';
 import MenuIcon from '@mui/icons-material/Menu';
 
 function Navbar() {
@@ -23,6 +24,17 @@ function Navbar() {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Mathmate
           </Typography>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+            onClick={()=>{ window.open('https://github.com/Mathmate-il', '_blank', 'noreferrer');}}
+          
+          >
+            <GitHubIcon />
+          </IconButton>
           <Button color="inherit" variant="outlined">Sign In</Button>
         </Toolbar>
       </AppBar>
