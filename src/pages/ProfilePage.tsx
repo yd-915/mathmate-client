2f3cdbc8c7fd1f0799e842100d26040e0dd96ce0
Erez Birenholz <biran330@gmail.com>
2023-01-29 02:52:58 +0200

    Take profile use from the store

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,18 +1,22 @@
 import { Stack, Avatar, Button } from "@mui/material";
 import SettingsIcon from "@mui/icons-material/Settings";
 import LogoutIcon from "@mui/icons-material/Logout";
+import useStore, { StoreState } from "../store/store";
+import { User } from "../typescript";
 
 function Profile() {
+  const currentUser: User = useStore((state: StoreState) => state.currentUser);
+
   return (
     <Stack spacing={2}>
       <Stack direction="row" spacing={2}>
         <Stack spacing={2}>
-          <h3>Full Name</h3>
-          <h5>email@gmail.com</h5>
+          <h3>{currentUser.name}</h3>
+          <h5>{currentUser.email}</h5>
         </Stack>
         <Avatar
-          alt="Remy Sharp"
-          src="https://mui.com/static/images/avatar/1.jpg"
+          alt={currentUser.name}
+          src={currentUser.image}
           sx={{ width: 150, height: 150 }}
         />
       </Stack>
--- src/store/auth-slice.ts
+++ src/store/auth-slice.ts
@@ -1,9 +1,17 @@
 import { StateCreator } from "zustand";
+import { initialUserObject, User } from "../typescript";
 
 export interface AuthSlice {
   isLoggedIn: boolean;
+  currentUser: User;
 }
 
 export const createAuthSlice: StateCreator<AuthSlice> = (set) => ({
   isLoggedIn: true,
+  currentUser: {
+    ...initialUserObject,
+    name: "Michael Jorden",
+    Image: "https://mui.com/static/images/avatar/1.jpg",
+    email: "mymail@gmail.com",
+  },
 });
