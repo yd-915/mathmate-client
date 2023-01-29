11288e7c571c9c4ad242d2b35073ea0cdff92419
Erez Birenholz <biran330@gmail.com>
2023-01-29 02:44:52 +0200

    use questions from the store

--- src/pages/SearchResultPage.tsx
+++ src/pages/SearchResultPage.tsx
@@ -1,39 +1,15 @@
 import { Card, Container, Stack } from "@mui/material";
-import { initialQuestionObject, Question } from "../typescript";
+import { Question } from "../typescript";
 import SearchInput from "../components/searchInput";
 import { MuiChipsInput } from "mui-chips-input";
-import { useState } from "react";
 import useStore, { StoreState } from "../store/store";
-
-const TEST_CONTENT = `Natus beatae eaque adipisci perspiciatis aliquam et enim sed et enim sed 
-aliquam et enim sed et enim sed jsfgku
-occaecati beatae eaque adipisc....`;
-
-const questions: Question[] = [
-  {
-    ...initialQuestionObject,
-    id: "1",
-    title: "QUESTION TITLE 1",
-    question: TEST_CONTENT,
-  },
-  {
-    ...initialQuestionObject,
-    id: "2",
-    title: "QUESTION TITLE 2",
-    question: TEST_CONTENT,
-  },
-  {
-    ...initialQuestionObject,
-    id: "3",
-    title: "QUESTION TITLE 3",
-    question: TEST_CONTENT,
-  },
-];
 
 function SearchResult() {
   const selectedQuestion = useStore(
     (state: StoreState) => state.selectedQuestion
   );
+
+  const questions: Question[] = useStore((state: StoreState) => state.questions);
 
   const handleChipsChange = (newTags: string[]) => {};
 
