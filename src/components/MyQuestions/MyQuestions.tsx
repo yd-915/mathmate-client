b2d04f27ce4cc42df3e158af947dcce2728e15e1
Erez Birenholz <biran330@gmail.com>
2023-02-13 17:24:26 +0200

    feat: Profile page (#36)
    * add some styles to the profile page
    
    * updae eslint
    
    * use store data
    
    * style the profile card
    
    * remove contributors
    
    * update readmie
    
    * create my questions component

---
+++ src/components/MyQuestions/MyQuestions.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function MyQuestions() {
+  return (
+    <div>MyQuestions</div>
+  )
+}
+
+export default MyQuestions
--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -5,24 +5,37 @@ import {
 } from '../../components/TabsControls/TabsControl';
 import Footer from '../../components/Footer';
 import ProfileCard from '../../components/Profile/ProfileCard';
+import SettingsIcon from '../../assets/settings.svg';
+import LogoutIcon from '../../assets/logout.svg';
+
 
 export default function Profile() {
   return (
     <div className="profile">
       <div>
         <ProfileCard />
-        <TabsControl>
-          <TabItem label="My Bookmarks">
-            <div>Bookmarks</div>
-          </TabItem>
-          <TabItem label="My Questions">
-            <div>Questions</div>
-          </TabItem>
-          <TabItem label="My Answers">
-            <div>Answers</div>
-          </TabItem>
-        </TabsControl>
+        <div className="mainArea">
+          <TabsControl>
+            <TabItem label="My Bookmarks">
+              <div>Bookmarks</div>
+            </TabItem>
+            <TabItem label="My Questions">
+              <div>Questions</div>
+            </TabItem>
+            <TabItem label="My Answers">
+              <div>Answers</div>
+            </TabItem>
+          </TabsControl>
+        </div>
         <hr />
+        <div className="actionBtn">
+          <img src={SettingsIcon} alt="settings" />
+          <span className="btnText">Settings</span>
+        </div>
+        <div className="actionBtn">
+          <img src={LogoutIcon} alt="logout" />
+          <span className="btnText">Logout</span>
+        </div>
       </div>
       <Footer />
     </div>
